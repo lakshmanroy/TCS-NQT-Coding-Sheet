1dc3b23a6ee50bdf786d7e9bbd496d9a72a81c5b
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-10-03 18:20:35 +0530

    commit

---
+++ TCS NQT Sheet/SmallestAndSecondSmallest.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+
+public class SmallestAndSecondSmallest {
+    public static void main(String[] args) {
+        int arr[]= {2, 4, 3, 5, 6};
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0]+ " ");
+        System.out.println(arr[1]+ " ");
+    }
+}
