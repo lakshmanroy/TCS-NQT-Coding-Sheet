a6e7ae75ff477c1902fdefeaebc893114e0c3e72
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-12-21 12:00:33 +0530

    commit

---
+++ TCS NQT Sheet/MaxProduct_SubArrays.java
@@ -0,0 +1,29 @@
+public class MaxProduct_SubArrays {
+    public static int maxProduct(int nums[]){
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+        int maxProduct=nums[0];
+        int currMax=nums[0];
+        int currMin=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+           int current=nums[i];
+           if(current<0){
+            int temp=currMax;
+            currMax=currMin;
+            currMin=temp;
+           }
+            // Update currentMax and currentMin
+            currMax=Math.max(current, currMax*current);
+            currMin=Math.min(current,currMin*current);
+
+            maxProduct=Math.max(maxProduct, currMax);
+        }
+        return maxProduct;
+    }
+    public static void main(String[] args) {
+        int nums[]={2,3,-2,4};
+        System.out.println("Maximum product :"+ maxProduct(nums));
+    }
+}
